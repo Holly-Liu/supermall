e0b9f2f97f1d647139210c5aa9a21522f660a960
liuhongli <holly@bupt.edu.cn>
2020-03-14 08:49:09 +0800

    首页导航,轮播图,推荐

---
+++ src/views/home/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <nav-bar class="nav-bar">
+      <span slot="center">购物街</span>
+    </nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
+  </div>
+</template>
+
+<script>
+  import NavBar from 'common/navbar/NavBar'
+  import HomeSwiper from "./childComps/HomeSwiper";
+  import RecommendView from "./childComps/RecommendView";
+
+
+  import {getHomeMultidata} from "../../network/home";
+
+  export default {
+    name: "Cart",
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    components: {
+      NavBar,
+      HomeSwiper,
+      RecommendView
+    },
+    created() {
+      getHomeMultidata().then(
+          res => {
+            this.banners = res.data.banner.list
+            this.recommends = res.data.recommend.list
+            console.log(res);
+          }
+      )
+    }
+  }
+</script>
+
+<style scoped>
+  .nav-bar {
+    background-color: var(--color-tint);
+    color: #fff;
+  }
+</style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,35 @@
+<template>
+    <swiper v-if="banners.length">
+      <swiper-item v-for="item in banners">
+        <a :href="item.link">
+          <img :src="item.image">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+  import Swiper from "../../../components/common/swiper/Swiper";
+  import SwiperItem from "../../../components/common/swiper/SwiperItem";
+
+  export default {
+    name: "HomeSwiper",
+    props: {
+      banners: {
+        type: Array,
+        default: []
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    },
+    methods: {
+
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
